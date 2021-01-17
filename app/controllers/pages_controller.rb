e12a97a0b6faf261db6553b06710c184f25cf3c2
bquinch <b.quinchon@talentoday.com>
2021-01-17 20:38:39 +0100

    [B] Skip auth for sendgrid

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :sendgrid]
   protect_from_forgery except: [:sendgrid]
 
   def home
