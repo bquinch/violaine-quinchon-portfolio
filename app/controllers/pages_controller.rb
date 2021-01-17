ee2aa30d88ac0d0b94af9df3c7f0b6d483d612db
bquinch <b.quinchon@talentoday.com>
2021-01-17 20:26:32 +0100

    [F] Config sendgrid webhooks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,15 @@ class PagesController < ApplicationController
     @experiences = Experience.all.order('start_date desc')
     @message = Message.new
   end
+
+  def sendgrid_webhooks
+    params["_json"].each do |payload|
+      @message.create(
+        subject: "Sendgrid response",
+        content: payload["smtp-id"],
+        name: "SG_WEBHOOK",
+        email: ENV["DEVELOPER_EMAIL_ADDRESS"]
+      )
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  post '/sendgrid', action: :sendgrid_webhooks, controller: "pages"
+
   resources :messages, only: :create
   resources :experiences, only: %i[index new create edit update destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
